9b34ccb079a98e67480a181aa08fd9f188288d2d
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2024-05-08 11:38:42 +0300

    View model для раздела настроек

---
+++ app/src/main/java/com/example/scheduletrackervyatsu/domain/SettingsViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.scheduletrackervyatsu.domain
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SettingsViewModel(
+    application: Application
+): AndroidViewModel(application) {
+
+
+}
